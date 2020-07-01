b139a6399080d96e58eec0e7fdfb074d51472ae2
jacobHildebrandt <jake@hildy.ca>
2020-07-01 10:54:58 -0700

    Added Player Horizontal Movement

--- Maplestory/Assets/Scripts/PlayerMovement.cs
+++ Maplestory/Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float speed = 10.0f;
 
     // Cached component references
     Rigidbody2D myRigidbody;
+
+    private Vector2 initialLocalScale;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        initialLocalScale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
+        FlipSprite();
+    }
+
+    private void Run()
+    {
+        float controlThrow = Input.GetAxis("Horizontal"); // -1 to +1
+        Vector2 playerVelocity = new Vector2(controlThrow * speed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+    }
+
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = IsRunning();
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(initialLocalScale.x * -Mathf.Sign(myRigidbody.velocity.x), initialLocalScale.y);
+        }
+    }
+
+    private bool IsRunning()
+    {
+        return Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
     }
 }
