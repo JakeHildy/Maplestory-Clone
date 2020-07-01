e50a43ec636e9ea965cc12310aedbe4d0401acdf
jacobHildebrandt <jake@hildy.ca>
2020-07-01 10:44:39 -0700

    Finished Player Idle Animation

---
+++ Maplestory/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // Cached component references
+    Rigidbody2D myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
